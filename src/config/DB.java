36f82a071acf61002a28f8bb79d02eeeea1f00eb
Brin <changwu0101@163.com>
2018-01-25 17:28:34 +0800

    Initial commit

---
+++ src/config/DB.java
@@ -0,0 +1,11 @@
+package config;
+
+ public class  DB {
+	 
+	 
+	 public static final String url = "jdbc:mysql://localhost:3306/spdb1";
+	 public static final String driver = "com.mysql.jdbc.Driver";  
+	 public static final String user = "root";  
+	 public static final String password = "brin";  
+
+}
